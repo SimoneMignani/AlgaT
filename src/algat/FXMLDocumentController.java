67d1491760dd2d7008485786cd72bd0315462710
Migno <migno97@libero.it>
2019-11-19 16:44:22 +0100

    homepage

--- src/algat/FXMLDocumentController.java
+++ src/algat/FXMLDocumentController.java
@@ -9,8 +9,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -22,10 +27,16 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    public void iniziamo(ActionEvent event) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("caso.fxml"));
+        Scene newScene = new Scene(parent);
+
+        //prendiamo le informazioni di Stage
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
+        window.setScene(newScene);
+        window.show();
     }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {

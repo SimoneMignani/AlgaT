d33d82158d1618c1ca41d3680c6a2d6f80a98357
Bassanelli Francesco <frebassa97@gmail.com>
2019-11-20 15:56:18 +0100

    prima lezione grafi con immagini e primo quiz

--- src/algat/LezioneUnoController.java
+++ src/algat/LezioneUnoController.java
@@ -5,15 +5,19 @@
  */
 package algat;
 
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -22,8 +26,26 @@ import javafx.stage.Stage;
  * @author Francesco
  */
 public class LezioneUnoController implements Initializable {
-    
-     //metodo che cambia la scene quando il bottone viene premuto
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView arco;
+
+    @FXML
+    private ImageView grafo_non_orientato;
+
+    @FXML
+    private ImageView grafo_orientato;
+
+    @FXML
+    private ImageView grafo_base;
+
+    //metodo che cambia la scene quando il bottone viene premuto
     public void backToLessonsList(ActionEvent event) throws IOException {
         Parent lezioneUNO = FXMLLoader.load(getClass().getResource("lessonsList.fxml"));
         Scene newScene = new Scene(lezioneUNO);
@@ -34,10 +56,10 @@ public class LezioneUnoController implements Initializable {
         window.setTitle("AlgaT - Lista delle lezioni");
         window.show();
     }
-    
-     //metodo che cambia la scene quando il bottone viene premuto
+
+    //metodo che cambia la scene quando il bottone viene premuto
     public void nextToQuiz(ActionEvent event) throws IOException {
-        Parent lezioneUNO = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent lezioneUNO = FXMLLoader.load(getClass().getResource("quizUnoLezioneUno.fxml"));
         Scene newScene = new Scene(lezioneUNO);
 
         //prendiamo le informazioni di Stage
@@ -52,7 +74,18 @@ public class LezioneUnoController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        Image gb = new Image("file:img/grafo_base.jpg");
+        grafo_base.setImage(gb);
+
+        Image arc = new Image("file:img/arco.jpg");
+        arco.setImage(arc);
+
+        Image go = new Image("file:img/grafo_orientato.jpg");
+        grafo_orientato.setImage(go);
+
+        Image gno = new Image("file:img/grafo_non_orientato.jpg");
+        grafo_non_orientato.setImage(gno);
+    }
+
 }
---
+++ src/algat/QuizUnoLezioneUnoController.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Francesco
+ */
+public class QuizUnoLezioneUnoController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private ToggleGroup quiz;
+
+    @FXML
+    private Label msg;
+
+    @FXML
+    private Button btnNextUno;
+
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private RadioButton opz1;
+
+    @FXML
+    private RadioButton opz2;
+
+    @FXML
+    private RadioButton opz3;
+
+    @FXML
+    private RadioButton opz4;
+
+    //metodo che cambia la scene quando il bottone viene premuto
+    public void backToLezioneGrafi(ActionEvent event) throws IOException {
+        Parent lezioneUNO = FXMLLoader.load(getClass().getResource("lezioneUno.fxml"));
+        Scene newScene = new Scene(lezioneUNO);
+
+        //prendiamo le informazioni di Stage
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(newScene);
+        window.setTitle("AlgaT - Lezione Grafi");
+        window.show();
+    }
+
+    @FXML
+    void radioSelect(ActionEvent event) {
+        RadioButton radio = (RadioButton) quiz.getSelectedToggle();
+        System.out.println(radio.getText() + " ID: " + radio.getId());
+        String messaggio = "";
+        String selected = "";
+        selected = radio.getId();
+        if (selected.equals("opz1")) {
+            messaggio += "Risposta errata !";
+        } else if (selected.equals("opz2")) {
+            messaggio += "Risposta corretta !";
+            btnNextUno.setDisable(false);
+        } else if (selected.equals("opz3")) {
+            messaggio += "Risposta errata !";
+        } else if (selected.equals("opz4")) {
+            messaggio += "Risposta errata !";
+        } 
+        msg.setText(messaggio);
+    }
+
+    @FXML
+    void radioHasChanged(ActionEvent event) {
+        btnConfirm.setDisable(false);
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        Image gb = new Image("file:img/grafo_esempio.jpg");
+        img.setImage(gb);
+    }
+
+}
